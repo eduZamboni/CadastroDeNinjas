e20ff68b7a84e4b2a84a7932fa15dd444de16d4e
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2025-12-27 23:39:07 -0300

    feat: Place Lombok in the class for abstraction.

--- src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,10 +2,16 @@ package com.projeto.CadastroDeNinjas.Ninjas;
 
 import com.projeto.CadastroDeNinjas.Missoes.MissaoModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 // Entity transforma uma classe em uma entidade do DB.
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -19,46 +25,4 @@ public class NinjaModel {
     @JoinColumn(name = "missoes_id") // Foreing Key ou chave estrangeira
     private MissaoModel missoes;
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade, MissaoModel missoes) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-        this.missoes = missoes;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public MissaoModel getMissoes() {
-        return missoes;
-    }
-
-    public void setMissoes(MissaoModel missoes) {
-        this.missoes = missoes;
-    }
 }

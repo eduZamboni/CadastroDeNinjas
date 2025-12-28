38efeda86a07fd56c025b1c7fae8803f6c73e836
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2025-12-28 19:11:40 -0300

    fix: Implement NinjaRepository and adds @Column to NinjaModel and MissoesModel.

--- src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaRepository.java
+++ src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaRepository.java
@@ -1,4 +1,7 @@
 package com.projeto.CadastroDeNinjas.Ninjas;
 
-public interface NinjaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NinjaRepository extends JpaRepository<NinjaModel, Long> {
+
 }

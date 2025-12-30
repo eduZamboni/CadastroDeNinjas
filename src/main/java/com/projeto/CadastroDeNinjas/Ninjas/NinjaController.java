f75f6585435b572fc4035e4634109f328103fe10
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2025-12-30 16:56:31 -0300

    feat: modifies the controller route to list the ninjas and injects the @Service and repository dependencies.

--- src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -2,9 +2,17 @@ package com.projeto.CadastroDeNinjas.Ninjas;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/ninjas")
 public class NinjaController {
+
+    private NinjaService ninjaService;
+
+    public NinjaController(NinjaService ninjaService) {
+        this.ninjaService = ninjaService;
+    }
 
     @GetMapping("/boasvindas")
     public String boasVindas() {
@@ -19,8 +27,8 @@ public class NinjaController {
 
     // Listar todos os ninjas (READ)
     @GetMapping("/listar")
-    public String listarNinjas(){
-        return "Listando todos os ninjas";
+    public List<NinjaModel> listarNinjas(){
+        return ninjaService.listarNinjas();
     }
 
     // Listar os ninjas por id (READ)

9baa95fe84d2241d19f93baded32ecd7c8c866f9
eduZamboni <128424472+eduZamboni@users.noreply.github.com>
2025-12-31 19:40:18 -0300

    feat: Includes the DTO as am abstraction for my entity in the Service and controller layers.

--- src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaService.java
+++ src/main/java/com/projeto/CadastroDeNinjas/Ninjas/NinjaService.java
@@ -8,9 +8,11 @@ import java.util.Optional;
 public class NinjaService {
 
     private NinjaRepository ninjaRepository;
+    private NinjaMapper ninjaMapper;
 
-    public NinjaService(NinjaRepository ninjaRepository) {
+    public NinjaService(NinjaRepository ninjaRepository, NinjaMapper ninjaMapper) {
         this.ninjaRepository = ninjaRepository;
+        this.ninjaMapper = ninjaMapper;
     }
 
     // Listar todos os ninjas
@@ -25,8 +27,10 @@ public class NinjaService {
     }
 
     // Criar um novo ninja
-    public NinjaModel criarNinja(NinjaModel ninjaModel){
-        return ninjaRepository.save(ninjaModel);
+    public NinjaDTO criarNinja(NinjaDTO ninjaDTO){
+        NinjaModel ninja = ninjaMapper.map(ninjaDTO);
+        ninja = ninjaRepository.save(ninja);
+        return ninjaMapper.map(ninja);
     }
 
     // Deletar o ninja - Tem que ser um metodo VOID
